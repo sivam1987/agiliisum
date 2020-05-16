71f996562e8d1a6d7a0bc76bb60f474fcd036e23
Siva Prasad <sivam198706@gmail.com>
2020-05-16 04:33:54 -0700

    this is Agilisum Test

---
+++ agiliisum-int-test/src/main/java/com/aglsum/test/api/AgiliisumIntTestApplication.java
@@ -0,0 +1,13 @@
+package com.aglsum.test.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgiliisumIntTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgiliisumIntTestApplication.class, args);
+	}
+
+}
